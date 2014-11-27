dd3e76b6cc50f0ed92bf6ef50d5274d816591490
Madeleine Dawson <mdd@mit.edu>
2014-11-26 20:45:31 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Madeleine Dease Dawson on 11/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
