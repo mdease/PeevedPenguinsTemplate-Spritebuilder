f83e294353135959f88be131c9d1da8a5b8c9fb8
Madeleine Dawson <mdd@mit.edu>
2014-11-28 02:13:25 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -14,13 +14,13 @@
 {
     // generate a random number between 0.0 and 2.0
     float delay = (arc4random() % 2000) / 1000.f;
+    printf("XXXXXXXXXXXXx");
     // call method to start animation after random delay
     [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
 - (void)startBlinkAndJump
 {
-    printf("blah");
     // the animation manager of each node is stored in the 'animationManager' property
     CCAnimationManager* animationManager = self.animationManager;
     // timelines can be referenced and run by name
